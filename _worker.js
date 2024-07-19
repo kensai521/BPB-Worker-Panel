0a4fe0b288c613f9d60b817d75f4231282570195
kensai521 <141028032+kensai521@users.noreply.github.com>
2024-07-19 15:25:50 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '8570ab94-dafb-453c-d8b2-4fff49e2309a';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
